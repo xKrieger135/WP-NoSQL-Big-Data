10bec59e26eb84bac653c300a5ac5aa77247ba76
Patrick Steinhauer <patrick.steinhauer@haw-hamburg.de>
2016-03-31 16:20:07 +0200

    Try to create relationships between the modules.

--- Aufgabe5/neo4jdatabase/Neo4J.py
+++ Aufgabe5/neo4jdatabase/Neo4J.py
@@ -3,39 +3,39 @@ from py2neo import Graph, Path
 from neo4jrestclient.client import GraphDatabase
 
 neoDB = GraphDatabase("http://localhost:7474", username="neo4j", password="master")
+graph = Graph()
 modulList = []
 relationshipList = []
 
 def readData():
-    with open("/home/nosql/Documents/WP-NoSQL-Big-Data/Doc/aimodules.graph", "r") as file:
+    # with open("/home/nosql/Documents/WP-NoSQL-Big-Data/Doc/aimodules.graph", "r") as file:
+    with open("D:\Uni\WP-NoSQL-Big-Data\Doc\\aimodules.graph", "r") as file:
         lines = []
         for line in file:
             lines.append(line)
     return lines
 
 def createLabels():
-    label = neoDB.labels.create("Modul")
+    label = neoDB.labels.create("teeeeeghfgeset")
     return label
 
 def createNodes():
-    i = 0
+    label = createLabels()
+    c = graph.cypher.begin()
     for module in readData():
         data = json.loads(module)
 
         modulename = data['Modulname']
-        modulList.append(modulename)
-        print modulList
-    print modulList
-print  createNodes()
+        relationship = data['Vorraussetzung']
+        m = neoDB.nodes.create(name=modulename)
+        label.add(m)
 
-# def creategraph():
-#     graph = Graph()
-#     cipher = graph.cypher.begin()
-#
-#     for module in readData():
-#         data = json.loads(module)
-#
-#         modul = data['Modulname']
-#         vorraussetzung = data['Vorraussetzung']
-#
-#         cipher.append("CREATE (modul:Modul {modulname:{modul}})")
+    for vorraussetzung in readData():
+        data = json.loads(vorraussetzung)
+
+        modul = data['Modulname']
+        vorraussetzungen = data['Vorraussetzung']
+
+
+
+print  createNodes()
