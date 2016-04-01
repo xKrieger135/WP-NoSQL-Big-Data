4dd192c5c139764ac95c521fa28a36468bb4468b
Patrick Steinhauer <patrick.steinhauer@haw-hamburg.de>
2016-04-01 16:52:02 +0200

    Try something.

--- Aufgabe5/neo4jdatabase/Neo4J.py
+++ Aufgabe5/neo4jdatabase/Neo4J.py
@@ -3,12 +3,13 @@ from py2neo import neo4j, Graph, Path, Node, Relationship
 from neo4jrestclient.client import GraphDatabase
 
 neoDB = GraphDatabase("http://localhost:7474", username="neo4j", password="master")
+graph = Graph()
 modulList = []
 relationshipList = []
 nodeList = []
 def readData():
-    with open("/home/nosql/Documents/WP-NoSQL-Big-Data/Doc/aimodules.graph", "r") as file:
-    # with open("D:\Uni\WP-NoSQL-Big-Data\Doc\\aimodules.graph", "r") as file:
+    # with open("/home/nosql/Documents/WP-NoSQL-Big-Data/Doc/aimodules.graph", "r") as file:
+    with open("D:\GitHub\WP-NoSQL-Big-Data\Doc\\aimodules.graph", "r") as file:
         lines = []
         for line in file:
             lines.append(line)
@@ -20,7 +21,7 @@ def createLabels():
 
 def createNodes():
     #TODO Vielleicht eine Klasse fuer Nodes, um die jeweiligen nodes speichern zu koennen um dann darauf zugreifen zu koennen
-    # label = createLabels()
+    label = createLabels()
     for module in readData():
         data = json.loads(module)
 
@@ -30,7 +31,31 @@ def createNodes():
 
         nodeList.append(node)
         # m = neoDB.nodes.create(name=modulename)
-        # label.add(m)
+        # label.add(node)
+
+    for m in readData():
+        data = json.loads(m)
+        name = data['Modulname']
+        rel = data['Vorraussetzung']
+        print nodeList.__contains__(Node("Modulname", name))
+        print "HI"
+        for r in rel:
+            print r
+            x = nodeList.__getitem__(Node("Modulname", name))
+            y = nodeList.__getitem__(Node("Modulname", r))
+            Node.cast(x)
+            relationship = Relationship(x, "needs", y)
+            if(graph.__contains__(Node("Modulname", x))):
+                graph.create(y)
+                graph.create(relationship)
+                print "Y"
+            else:
+                graph.create(x)
+                graph.create(relationship)
+                print "X"
+
+
+    print nodeList.__contains__(Node("Module", "GI"))
     print nodeList
 print  createNodes()
 
