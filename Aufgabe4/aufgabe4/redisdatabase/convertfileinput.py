0393fd04c0968e1be608df42e17e913482b6d4d6
nosql <patrick.steinhauer@haw-hamburg.de>
2016-04-12 08:18:36 -0700

    Add function to get city and state for postal code. Some bug fixes.

--- Aufgabe4/aufgabe4/redisdatabase/convertfileinput.py
+++ Aufgabe4/aufgabe4/redisdatabase/convertfileinput.py
@@ -16,17 +16,26 @@ class redis_db:
     def importDataToRedis(self):
         for object in self.readData():
             data = json.loads(object)
-            key = data['_id'].encode("utf8")
-            print(data)
-            v = data
+            key = data['_id']
+            if '_id' in data:
+                del data['_id']
             value = data['city']
-            self.redis_connection.set(key, value)
+            self.redis_connection.set(key, data)
             # rpush adds values to the same town
             self.redis_connection.rpush(value, key)
 
     def searchTownByPLZ(self, plz):
-        town = self.redis_connection.get(plz)
-        return town
+        # TODO: Try to find another solution for decoding bytes from redis to the correct JSON object!
+        redis_data = self.redis_connection.get(plz)
+        decoded_data = str(redis_data, 'utf-8')
+        correct_decoded_data = decoded_data.replace("'", '"')
+        decoded_json_data = json.dumps(correct_decoded_data)
+        json_data = json.loads(decoded_json_data)
+        correct_json_data = json.loads(json_data)
+        results = []
+        results.append(correct_json_data['city'])
+        results.append(correct_json_data['state'])
+        return results
 
     def searchPLZByTown(self, city):
         listWithPLZ = []
@@ -46,20 +55,19 @@ def show_database_entries():
     db = redis_db()
     return db.show_entries()
 
-@app.route('/plz')
-def show_city():
+@app.route('/postalcode')
+def show_city_and_state():
     db = redis_db()
-    plz = request.args['plz']
-    city = [dict(CITY=db.searchTownByPLZ(plz))]
-    print(city)
+    postalcode = request.args['postalcode']
+    city = [dict(CITY=db.searchTownByPLZ(postalcode))]
     return render_template('index.html', city=city)
 
 @app.route('/city')
-def show_plz():
+def show_postalcode():
     db = redis_db()
     city = request.args['city']
-    plz = [dict(PLZ=db.searchPLZByTown(city))]
-    return render_template("index.html", plz=plz)
+    postalcode = [dict(POSTALCODE=db.searchPLZByTown(city))]
+    return render_template("index.html", postalcode=postalcode)
 
 @app.route('/import')
 def import_data():
