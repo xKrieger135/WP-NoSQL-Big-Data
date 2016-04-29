d80903241175ce79b22c715af278f4756561d156
root <patrick.steinhauer@haw-hamburg.de>
2016-04-29 02:19:44 -0700

    fix mongodb implementation, edit doc and fix insert data for fussball (date)

--- NoSQL/haw/mongodb/Mongo_DB.py
+++ NoSQL/haw/mongodb/Mongo_DB.py
@@ -8,51 +8,21 @@ class Mongo_DB():
     mongodb_collection = None
 
     def read_data(self):
-        # with open("/home/nosql/Downloads/plz.data", "r") as file:
-        with open("/home/nosql/Downloads/sinndeslebens.txt", "r") as file:
+        with open("/home/nosql/Downloads/plz.data", "r") as file:
             lines = []
             for line in file:
                 lines.append(line)
         return lines
 
-    def get_import_data(self, data):
-        result = []
-        for insert_data in data:
-            cleaned_data = ""
-            insert_data_as_string = "".join(insert_data)
-            i = 0
-            while(i <= len(insert_data_as_string) - 1):
-                if(insert_data_as_string[i] == "{"):
-                    while(i != len(insert_data_as_string) - 3):
-                        cleaned_data += insert_data_as_string[i]
-                        i = i + 1
-                i = i + 1
-            result.append(cleaned_data)
-        return result
-
     def import_data_to_mongodb(self):
         if(not "nosql" in self.database.collection_names()):
             self.database.create_collection("nosql")
-        if(not "fussball" in self.database.collection_names()):
-            self.database.create_collection("fussball")
         self.database.drop_collection("nosql")
-        self.database.drop_collection("fussball")
         postalcode_collection = self.database.get_collection("nosql")
-        fussball_collection = self.database.get_collection("fussball")
 
-        if(postalcode_collection.full_name == "nosql"):
-            for elem in self.read_data():
-                data = json.loads(elem)
-                postalcode_collection.insert(data)
-        if(fussball_collection.name == "fussball"):
-            insert_data = self.get_import_data(self.read_data())
-            print(insert_data)
-            for elem in insert_data:
-                # To have automatically read from file and insert its neccessary to pull every data as an extra elem!
-                # This is only for automation processes (read {...} from method from sinndeslebens.txt
-                print(elem)
-                data = {'fussball_data' : elem}
-                fussball_collection.insert(data)
+        for elem in self.read_data():
+            data = json.loads(elem)
+            postalcode_collection.insert(data)
 
     def search_city_and_state_by_postalcode(self, postalcode):
         results = []
@@ -74,12 +44,3 @@ class Mongo_DB():
                 postalcode = elem['_id']
                 results.append(postalcode)
         return results
-
-    def search_football_team_name(self):
-        if("fussball" in self.database.collection_names()):
-            fussball = self.database.get_collection("fussball")
-            data = fussball.find({'fusball_data' : {'name' : 'HSV'}})
-            print(data)
-db = Mongo_DB()
-db.import_data_to_mongodb()
-db.search_football_team_name()
